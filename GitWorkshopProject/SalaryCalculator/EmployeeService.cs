e0dc46e0f5994837abdc96545849470edbdc4197
Phùng Hùng <phunghung29@gmail.com>
2022-03-03 08:18:55 +0700

    Task-02 3/3/22 8:19

--- GitWorkshopProject/SalaryCalculator/EmployeeService.cs
+++ GitWorkshopProject/SalaryCalculator/EmployeeService.cs
@@ -15,20 +15,39 @@ namespace SalaryCalculator
 
             decimal factor = 0;
 
-            if (employeeType == EmployeeType.Engineer)
+            // if (employeeType == EmployeeType.Engineer)
+            // {
+            //     factor = 1;
+            // }
+            // else if (employeeType == EmployeeType.SeniorEngineer || employeeType == EmployeeType.Manager)
+            // {
+            //     // factor = 1.5m;
+            //     factor = 2;
+            // }
+            // else if (employeeType == EmployeeType.SeniorManager)
+            // {
+            //     factor = 3;
+            // }
+            // // TODO: if employee is Expert, set factor = 3
+
+            switch (employeeType)
             {
-                factor = 1;
+                case EmployeeType.Engineer:
+                    factor = 1;
+                    break;
+                case EmployeeType.SeniorEngineer:
+                case EmployeeType.Manager:
+                    factor = 2;
+                    break;
+                case EmployeeType.SeniorManager:
+                    factor = 3;
+                    break;
+                case EmployeeType.Expert:
+                    factor = 3;
+                    break;
+                default:
+                    break;
             }
-            else if (employeeType == EmployeeType.SeniorEngineer || employeeType == EmployeeType.Manager)
-            {
-                // factor = 1.5m;
-                factor = 2;
-            }
-            else if (employeeType == EmployeeType.SeniorManager)
-            {
-                factor = 3;
-            }
-            // TODO: if employee is Expert, set factor = 3
 
             var salary = BASIC_SALARY * years * factor;
 
